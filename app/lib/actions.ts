17d113a4a22c6beabe00ab4c73dd069ec27e2e7e
Fernando Claverino <fernando.claverino@gmail.com>
2024-12-16 18:06:07 -0300

    fix deleteInvoice compilation error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -111,13 +111,13 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
+  // // throw new Error("Failed to Delete Invoice");
 
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-  } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice" };
-  }
+  // try {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // } catch (error) {
+  //   return { message: "Database Error: Failed to Delete Invoice" };
+  // }
 
   revalidatePath("/dashboard/invoices");
 }
